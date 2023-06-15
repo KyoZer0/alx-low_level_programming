f0b82d47d3cba3efca481df333dcc47b89ba3a13
Lahcen Aharouane <lahcen.aharouane@gmail.com>
2023-06-14 22:57:12 -0700

    fixed

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description : '4.Hello, puts'
+ *
+ * @void : The main function takes no parameters
+ * Return: Always 0 (Success)
+ */
 
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return 0;
+	return (0);
 }

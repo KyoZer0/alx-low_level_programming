aae74171cdff10bd2330dc1b624513f0c143fdc2
Lahcen Aharouane <lahcen.aharouane@gmail.com>
2023-06-15 01:41:37 -0700

    optional tasks

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * my_strlen - Counts and returns the length of the string passed to it.
+ *
+ * @str : The string to be counted.
+ *
+ * Return: returns the length of the string.
+ */
+int	my_strlen(char *str)
+{
+	int i;
+
+	i = 0;
+
+	while (str[i])
+		i++;
+	return (i);
+}
+
+/**
+ * main - Entry point
+ *
+ * @void : the main function takes no parameters.
+ *
+ * Return: always returns 1
+ */
+int	main(void)
+{
+	char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+
+	write(2, &str, my_strlen(str));
+	write(2, "\n", 2);
+	return (1);
+}

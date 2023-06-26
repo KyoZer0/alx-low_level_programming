37c3b911b3c5bcb0376d9ffb8e9184e3c989bb1f
Lahcen Aharouane <lahcen.aharouane@gmail.com>
2023-06-26 15:19:22 -0700

    pointers

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,39 @@
+/**
+ * my_strlen - takes a string and give the length
+ *
+ * @s: the string passed to the functions
+ *
+ * Return: the length of s
+ */
+int my_strlen(char *s)
+{
+	char *p = s;
+
+	while (*s)
+	{
+		s++;
+	}
+
+	return (s - p);
+}
+/**
+ * rev_string - reverses a string
+ *
+ * @s: takes a string
+ */
+void rev_string(char *s)
+{
+	int len = strlen(s);
+	int i = 0;
+	int j = len - 1;
+	char tmp;
+
+	while (i < j)
+	{
+		tmp = s[i];
+		s[i] = s[j];
+		s[j] = tmp;
+		i++;
+		j--;
+	}
+}

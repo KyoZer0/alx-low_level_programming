448473497d9d8a2dd52942f03b510298f8adc92d
Lahcen Aharouane <lahcen.aharouane@gmail.com>
2023-06-26 09:29:33 -0700

    fixed

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,13 +1,33 @@
 #include "main.h"
 /**
+ * my_strlen - counts the srting characters
+ *
+ * @s: takes a char s
+ *
+ * Return: return the length
+ */
+int my_strlen(char *s)
+{
+	char *p = s;
+
+	while (*s)
+	{
+		s++;
+	}
+
+	return (s - p);
+}
+
+/**
  * print_rev - prints the char in reverse
  *
  * @s: the string
  */
+
 void print_rev(char *s)
 {
 
-	int len = _strlen(s);
+	int len = my_strlen(s);
 
 	while (s[len] - 1)
 	{

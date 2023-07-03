26e6c85913b57da30ed75d11fd3b5cda14fba988
Lahcen Aharouane <lahcen.aharouane@gmail.com>
2023-07-03 12:15:18 -0700

    fix

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,3 +1,13 @@
+/**
+ * *_strchr - ocates a character in a string
+ *
+ * @s: the string
+ *
+ * @c: the character
+ *
+ * Return:  first occurrence of the character c in the string s,
+ * or NULL if the character is not found
+ */
 char *_strchr(char *s, char c)
 {
 	int i = 0;

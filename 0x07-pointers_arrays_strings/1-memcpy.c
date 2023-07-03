c3b086d7c61ccaa2054d29a52315598868e42504
Lahcen Aharouane <lahcen.aharouane@gmail.com>
2023-07-03 12:19:31 -0700

    fix

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -13,11 +13,10 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i = 0;
 
-	while (src[i] && n > 0)
+	while (src[i] && i < n)
 	{
 		dest[i] = src[i];
 		i++;
-		n--;
 	}
 	return (dest);
 }

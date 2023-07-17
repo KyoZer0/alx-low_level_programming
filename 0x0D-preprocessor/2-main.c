576791de5e6b4e93f57260657a51dce2633bf52a
Lahcen Aharouane <lahcen.aharouane@gmail.com>
2023-07-17 01:23:24 -0700

    pushing the files

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Entry point.
+ *
+ * @void: the program takes no args
+ *
+ * Return: always returns 0
+ */
+int main(void)
+{
+	printf("%s \n", __FILE__);
+	return (0);
+}
---
+++ 0x0D-preprocessor/README.md
@@ -0,0 +1,2 @@
+# 0x0D. C - Preprocessor
+This project contains tasks related to learning about the C Preprocessor in C programming.

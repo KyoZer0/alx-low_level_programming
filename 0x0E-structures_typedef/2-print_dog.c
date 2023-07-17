f1858e2840f2573753aeefb55c70d3c89e822bcf
Lahcen Aharouane <lahcen.aharouane@gmail.com>
2023-07-17 16:48:36 -0700

    push

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -1,11 +1,10 @@
-#include "dog.h"
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
 
 /**
- * print_dog - prints the struct.
- *
- * @d: the dog struct.
+ * print_dog - prints a struct dog
+ * @d: struct dog to print
  */
 void print_dog(struct dog *d)
 {
